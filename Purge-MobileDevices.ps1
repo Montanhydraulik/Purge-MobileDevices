ea144ee96e34ef6410d7464635a56ab241193309
Tim Gerundt <tim@gerundt.de>
2021-05-04 14:18:40 +0200

    Add MIT license to script

--- Purge-MobileDevices.ps1
+++ Purge-MobileDevices.ps1
@@ -1,3 +1,8 @@
+# Copyright (c) 2021 Tim Gerundt. All rights reserved.
+#
+# This work is licensed under the terms of the MIT license.
+# For a copy, see <https://opensource.org/licenses/MIT>.
+
 [CmdletBinding()]
 Param(
 	[Parameter(Mandatory=$false)]
